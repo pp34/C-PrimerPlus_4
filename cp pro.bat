3d64c66441d7972e38d6fe5d2a6ca0997a4e056a
rd257 <rd257@zy-ivd.com>
2018-01-08 16:10:58 +0800

    Add a .bat for copy automatical

---
+++ cp pro.bat
@@ -0,0 +1,5 @@
+copy F:\homework\C-PrimerPlus_3.7\cpluswork\cpluswork.sln  F:\Github\C-PrimerPlus_4 /h/y
+copy F:\homework\C-PrimerPlus_3.7\cpluswork\cpluswork.VC.db  F:\Github\C-PrimerPlus_4 /h/y
+copy F:\homework\C-PrimerPlus_3.7\cpluswork\cpluswork.VC.VC.opendb  F:\Github\C-PrimerPlus_4 /h/y
+xcopy F:\homework\C-PrimerPlus_3.7\cpluswork\cpluswork  F:\Github\C-PrimerPlus_4\cpluswork /h/y
+xcopy F:\homework\C-PrimerPlus_3.7\cpluswork\Debug  F:\Github\C-PrimerPlus_4\Debug /h/y
--- cpluswork/main.cpp
+++ cpluswork/main.cpp
@@ -1,45 +1,23 @@
 #include "main.h"
 
 
-typedef char *pstring;
+#define tellmetype(o)   std::cout<<#o<<"\t: "<<typeid(o).name()<<std::endl;
+//  #o output its name;
+//   o output its value;
 
 int main(){
 
-    int i = 0, &r = i;
-    auto a = r;                     //  a int value
-    std::cout << "a: " << a << std::endl;
-    const int ci = i, &cr = ci;    //  ci top-level constant int value, cr constant int reference.
-    std::cout << "ci: " << ci << std::endl;
-    std::cout << "cr " << cr << std::endl;
-    auto b = ci;                    //  b int value(ignore ci's top-level constant).
-    std::cout << "b: " << b << std::endl;
-    auto c = cr;                    //  c int value(ignore ci's top-level constant).
-    std::cout << "c: " << c << std::endl;
-    auto d = &i;                    //  d int pointer(pointer to int value).
-    std::cout << "*d: " << *d << std::endl;
-    auto e = &ci;                   //  e pointer to constant int value.
-    std::cout << "*e: " << *e << std::endl;
-    const auto f = ci;
-    std::cout << "f: " << f << std::endl;
-    auto &g = ci;
-    std::cout << "g: " << g << std::endl;
-    //auto &h = 42;     reference 
-    const auto &j = 42;
-    std::cout << "j: " << j << std::endl;
-    auto k = ci, &l = i;
-    std::cout << "k: " << k << std::endl;
-    std::cout << "l: " << l << std::endl;
-    auto &m = ci, *p = &ci;
-    std::cout << "m: " << m << std::endl;
-    std::cout << "*p: " << *p << std::endl;
-    a = 42;
-    b = 42;
-    c = 42;
-    //d = 42;
-    //e = 42;
-    //g = 42;
-    std::cout << "a: " << a << std::endl;
-    std::cout << "b: " << b << std::endl;
-    std::cout << "c: " << c << std::endl;
+    const int i = 42;
+    auto j = i;
+    const auto &k = i; auto *p = &i;
+    const auto j2 = i, &k2 = i;
+
+    tellmetype( i );
+    tellmetype( j );
+    tellmetype( k );
+    tellmetype( p );
+    tellmetype( j2 );
+    tellmetype( k2 );
+
     return 0;
 }
